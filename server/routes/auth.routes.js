7b8fbf601d0cd4f4a37d1cf22c754ffc0b751e07
vladimir konovalov <kvvma@bk.ru>
2022-02-22 22:50:55 +0300

    add auth work

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,138 @@
 const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const { generateUserData } = require("../utils/helpers");
+const tokenServise = require("../services/token.service");
 const router = express.Router({ mergeParams: true });
 
-router.post("/signUp", async (req, res) => {});
+router.post("/signUp", [
+  check("email", "Некорректная электронная почта").isEmail(),
+  check("password", "Минимальная длина пароля 8 символов").isLength({ min: 8 }),
 
-router.post("/signInWithPassword", async (req, res) => {});
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
 
-router.post("/token", async (req, res) => {});
+      if (!errors.isEmpty()) {
+        return res.status(400).json({
+          error: {
+            message: "INVALID_DATA",
+            code: 400,
+            // errors: errors.array(),
+          },
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(400).json({
+          error: {
+            message: "EMAIL_EXISTS",
+            code: 400,
+          },
+        });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      const newUser = await User.create({
+        ...generateUserData(),
+        ...req.body,
+        password: hashedPassword,
+      });
+
+      const tokens = tokenServise.generate({ _id: newUser.id });
+      await tokenServise.save(newUser._id, tokens.refreshToken);
+
+      res.status(201).send({ ...tokens, userId: newUser.id });
+    } catch (e) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка",
+      });
+    }
+  },
+]);
+
+router.post("/signInWithPassword", [
+  check("email", "Email не корректный").normalizeEmail().isEmail(),
+  check("password", "Пароль не может быть пустым").exists(),
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).send({
+          error: {
+            message: "INVALID_DATA",
+            code: 400,
+            errors: errors.array(),
+          },
+        });
+      }
+
+      const { email, password } = req.body;
+
+      const existingUser = await User.findOne({ email });
+
+      if (!existingUser) {
+        return res
+          .status(400)
+          .send({ error: { message: "EMAIL_NOT_FOUND", code: 400 } });
+      }
+
+      const isPasswordEqual = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+      if (!isPasswordEqual) {
+        return res
+          .status(400)
+          .send({ error: { message: "INVALID_PASSWORD", code: 400 } });
+      }
+
+      const tokens = tokenServise.generate({ _id: existingUser._id });
+      await tokenServise.save(existingUser._id, tokens.refreshToken);
+
+      res.status(200).send({ ...tokens, userId: existingUser._id });
+    } catch (e) {
+      res.status(500).json({
+        message: "На сервере произошла ошибка",
+      });
+    }
+  },
+]);
+
+function isTokenInvalid(data, dbToken) {
+  return !data || !dbToken || data._id !== dbToken?.user?.toString();
+}
+
+router.post("/token", async (req, res) => {
+  try {
+    const { refresh_token: refreshToken } = req.body;
+    const data = tokenServise.validateRefresh(refreshToken);
+
+    const dbToken = await tokenServise.findToken(refreshToken);
+
+    if (isTokenInvalid(data, dbToken)) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const tokens = await tokenServise.generate({
+      _id: data._id,
+    });
+
+    await tokenServise.save(data._id, tokens.refreshToken);
+
+    res.status(200).send({ ...tokens, userId: data._id });
+  } catch (e) {
+    res.status(500).json({
+      message: "На сервере произошла ошибка",
+    });
+  }
+});
 
 module.exports = router;
